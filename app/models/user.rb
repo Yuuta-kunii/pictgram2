1ba7d84f5d1f5b1a225b0a11de2ab84e8d796d2a
Yuuta-kunii <rikyu1@outlook.jp>
2021-04-08 23:10:25 +0000

    submit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-    validates :name, presence: true
-    validates :email, presence: true
+    validates :name, presence: true, length:{muxmum:15}
+    VALID_EMAIL_REGEX=/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true,format:{with:VALID_EMAIL_REGEX}
+    validate :password, length:{in: 8..32}
+    validate :password, format:{with: /\A(?=.&#042;?[a-z])(?=.&#042;?\d)[a-z\d]+\z/i }
+    validate :password, 
+
+    
     
     has_secure_password
 end
